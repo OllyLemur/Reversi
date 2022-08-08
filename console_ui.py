64fad2fb77b9358e9ae465dae6a556500ac259b8
Aleksandra Savich <savichaliaksandra@gmail.com>
2022-08-08 14:19:14 +0300

    add validation for usr input

--- console_ui.py
+++ console_ui.py
@@ -1,3 +1,5 @@
+import re
+
 from model.classic_game import ClassicGameRules
 from view.game_console_view import GameConsoleView
 from controller.game_controller import GameController
@@ -13,6 +15,7 @@ class ConsoleUI:
         view = GameConsoleView(model)
 
         game_mode = input('Select game mode: \n a. Player VS Player b. Player VS Computer \n input (a, b): ')
+        game_mode = self._is_valid_input(game_mode, f'[a,b]')
 
         if game_mode == 'a':
             player_one = HumanPlayer(1)
@@ -21,6 +24,7 @@ class ConsoleUI:
             controller.play_game()
         elif game_mode == 'b':
             game_difficulty = input('Select game difficulty: easy(e), medium(m), hard(h): ')
+            game_difficulty = self._is_valid_input(game_difficulty, f'[e,m,h]')
             player_one = HumanPlayer(1)
 
             if game_difficulty == 'e':
@@ -36,4 +40,15 @@ class ConsoleUI:
                 controller = GameController(view, model, player_one, player_two)
                 controller.play_game()
 
-            
+    def _is_valid_input(self, user_input, regex):
+        is_valid = bool(re.search(regex, user_input))
+
+        if is_valid:
+            return user_input
+        else:
+            while True:
+                print('Input error')
+                user_input = input('Select game mode: \n a. Player VS Player b. Player VS Computer \n input (a, b): ')
+                is_valid = bool(re.search(regex, user_input))
+                if is_valid:
+                    return user_input
